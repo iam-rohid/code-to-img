615b24315d0e1bf9d9f3029fe1a8e69c3282bd7c
Rohid <rohidul209@gmail.com>
2024-11-23 21:55:27 +0600

    dynamic=force-static

--- src/app/(dashboard)/[workspaceSlug]/layout.tsx
+++ src/app/(dashboard)/[workspaceSlug]/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 
 import WorkspaceProvider from "@/providers/workspace-provider";
 
+export const dynamic = "force-static";
+
 export default function WorkspaceLayout({ children }: { children: ReactNode }) {
   return <WorkspaceProvider>{children}</WorkspaceProvider>;
 }
